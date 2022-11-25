82a261552e8b45cb420e4f9b85cf6425468e26e6
GillianY1 <gilliangilliany@gmail.com>
2022-11-25 17:14:27 +0800

    83-2 input alias

--- pages/src/app/cust-class.directive.ts
+++ pages/src/app/cust-class.directive.ts
@@ -8,8 +8,18 @@ export class CustClassDirective {
 
   constructor(private element:ElementRef ) {}
 
-  @Input() set appCustClass(color:string){
+  @Input('appCustClass') set backgroundColor(color:string){
     this.element.nativeElement.style.backgroundColor = color;//this.backgroundColor;//'orange';
   }
+  /*
+  @Input('appCustClass') set ClassNames(classObj: any){
+    for(let key in classObj){
+      if(classObj[key]){
+        this.element.nativeElement.classList.add(key);
+      }else{
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+  }*/
 
 }
