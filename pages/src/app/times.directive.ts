e0d5e710f5dcd0a65d19221401a79830e7bc1522
GillianY1 <gilliangilliany@gmail.com>
2022-11-28 17:54:17 +0800

    customed structual directive

--- pages/src/app/times.directive.ts
+++ pages/src/app/times.directive.ts
@@ -14,7 +14,7 @@ export class TimesDirective {
     {
       this.viewContainerRef.clear();
       for(let i =0; i<times ; i++){
-        this.viewContainerRef.createEmbeddedView( this.templateRef, {})
+        this.viewContainerRef.createEmbeddedView( this.templateRef, {index:i})
       }
     }
 
