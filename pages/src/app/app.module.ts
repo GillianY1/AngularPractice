f89f7f2fbdab2bbedab0272643848acac94b59d7
GillianY1 <gilliangilliany@gmail.com>
2022-11-28 17:42:09 +0800

    add customed structure directive

--- pages/src/app/app.module.ts
+++ pages/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CustClassDirective } from './cust-class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustClassDirective
+    CustClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
